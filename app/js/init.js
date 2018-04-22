bd2b3b72f6b5753189c0cbe85c0863c5fc8f59f5
Nik <myller833@gmail.com>
2018-04-22 22:06:39 +0300

    сделал guide.html, dropdown.html/css добавил изменения в tags.css

--- app/js/init.js
+++ app/js/init.js
@@ -1,28 +1,10 @@
-//Select init
-$('select').styler();
+// Init Tabs
+$( function() {
+    $( "#tabs" ).tabs();
+} );
 
-// Slick carousel init
 $('.trending-carousel').slick({
-    true,
-    slidesToShow: 4,
-    slidesToScroll: 1,
-
-    arrows:false,
-    dots: true,
     infinite: true,
-    slidesToShow: 4,
-    slidesToScroll: 1
-});
-
-// Switch product items view
-$('.grid-thumbs').on('click', function(){
-    // Find cards and remove class row-card
-    $('.all-items-container .card').removeClass('row-card');
-    $('.all-items-container .col-4').removeClass('col-12');
-});
-
-$('.grid-list').on('click', function(){
-    // Find cards and add class row-card
-    $('.all-items-container .card').addClass('row-card');
-    $('.all-items-container .col-4').addClass('col-12');
+    slidesToShow: 3,
+    slidesToScroll: 3
 });
