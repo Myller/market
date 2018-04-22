3fdeac56d7b05f53852c9f4002492f164dee557d
OlgaM357 <stolgam17@gmail.com>
2018-04-22 18:08:07 +0300

    commit before pull

--- app/js/init.js
+++ app/js/init.js
@@ -16,10 +16,13 @@ $('.grid-thumbs').on('click', function(){
     // Find cards and remove class row-card
     $('.all-items-container .card').removeClass('row-card');
     $('.all-items-container .col-4').removeClass('col-12');
+    // $('.all-items-container .card').removeClass('card-text-info');
 });
 
 $('.grid-list').on('click', function(){
     // Find cards and add class row-card
     $('.all-items-container .card').addClass('row-card');
     $('.all-items-container .col-4').addClass('col-12');
+    // $('.all-items-container .card').addClass('card-text-info');
+
 });
