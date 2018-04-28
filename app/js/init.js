172f1221886b054d97c764450b2c88eca09ace03
OlgaM357 <stolgam17@gmail.com>
2018-04-28 17:54:07 +0300

    add modal.components and upload-products.page

--- app/js/init.js
+++ app/js/init.js
@@ -10,8 +10,11 @@ $('.trending-carousel').slick({
     dots: true
 });
 
+//Select init
+$('select').styler();
+
 // Switch product items view
-$('.grid-thumbs').on('click', function(){
+$('.grid-thumbs').on('click', function() {
     // Find cards and remove class row-card
     $('.all-items-container .card').removeClass('row-card');
     $('.all-items-container .col-lg-4').removeClass('col-lg-12');
